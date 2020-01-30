102786bf2924cd1db634ae94778cfa8a2c2e6cb6
Roman Vasilyev <sektor_wins@mail.ru>
2020-01-30 15:28:37 +0500

    Добавлен интерфейс для реализации репозитория для работы с БД

--- ReportService/ReportService/Domain/Interfaces.cs
+++ ReportService/ReportService/Domain/Interfaces.cs
@@ -40,4 +40,24 @@ namespace ReportService.Domain
         /// </summary>
         Action<Employee, Report> AddEmployeeDepartament { get; }
     }
+
+
+    /// <summary>
+    /// Базовый интерфейс для реализации паттерна репозитория для доступа к БД
+    /// </summary>
+    public interface IDatabaseRepository
+    {
+        /// <summary>
+        /// Возвращает сведения о всех сотрудниках в БД
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Employee> GetAllEmployees();
+
+        /// <summary>
+        /// Возвращает сведения о всех сотрудниках, сгруппированных по отделам
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<IGrouping<string, Employee>> GetEmployeesByDept();
+    }
+
 }
