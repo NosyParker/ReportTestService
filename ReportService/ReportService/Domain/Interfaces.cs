5ed7a1df11e3381119ad060b39b9ab19a2641415
Roman Vasilyev <sektor_wins@mail.ru>
2020-01-30 12:21:15 +0500

    Добавлен интерфейс для классов заполнения отчетов

---
+++ ReportService/ReportService/Domain/Interfaces.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportService.Domain
+{
+    /// <summary>
+    /// Базовый интерфейс классов для заполнения отчетов
+    /// </summary>
+    public interface IReportFormatter
+    {
+        /// <summary>
+        /// Добавление символа новой строки
+        /// </summary>
+        Action<Employee, Report> AddNewLine { get;}
+
+        /// <summary>
+        /// Добавление строки-разделителя
+        /// </summary>
+        Action<Employee, Report> AddDividerLine { get; }
+
+        /// <summary>
+        /// Добавление таба
+        /// </summary>
+        Action<Employee, Report> AddTabLine { get; }
+
+        /// <summary>
+        /// Добавление ФИО работника
+        /// </summary>
+        Action<Employee, Report> AddEmployeeName { get; }
+
+        /// <summary>
+        /// Добавление зарплаты работника
+        /// </summary>
+        Action<Employee, Report> AddEmployeeSalary { get; }
+
+        /// <summary>
+        /// Добавление отдела работника
+        /// </summary>
+        Action<Employee, Report> AddEmployeeDepartament { get; }
+    }
+}
