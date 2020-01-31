3f3fda7bbc039038fdac55c7743ead97d0973e23
Roman Vasilyev <sektor_wins@mail.ru>
2020-01-31 22:21:05 +0500

    Добавлен класс для получения набора объектов сотрудников, заполненных необходимыми данными

---
+++ ReportService/ReportService/Domain/EmployeeFillingData.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ReportService.Domain
+{
+    /// <summary>
+    /// Сервис для получения набора объектов сотрудников,
+    /// заполненных необходимыми данными
+    /// </summary>
+    public class EmployeeFillingData : IEmployeeFillingData
+    {
+        private readonly IDatabaseRepository repo;
+        private readonly ISalaryGetter salaryGetter;
+        private readonly IEmpCodeGetter empCodeGetter;
+
+        public EmployeeFillingData(IDatabaseRepository repository,
+                                    ISalaryGetter salaryGetterService,
+                                    IEmpCodeGetter empCodeGetterService)
+        {
+            repo = repository;
+            salaryGetter = salaryGetterService;
+            empCodeGetter = empCodeGetterService;
+        }
+
+        /// <summary>
+        /// Возвращает набор объектов сотрудников, 
+        /// заполненных данными о зарплате и кодах сотрудника
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<Employee>> GetEmployeesWithFilledData()
+        {
+            var employees = repo.GetAllEmployees();
+
+            foreach(var emp in employees)
+            {
+                emp.BuhCode = await empCodeGetter.GetBuhCode(emp.Inn);
+                emp.Salary = await salaryGetter.GetSalary(emp);
+            }
+
+            return employees;
+        }
+
+    }
+
+}
