29c9682e77789b5d33e7f374bc4f0d5b696ded21
Roman Vasilyev <sektor_wins@mail.ru>
2020-01-31 22:19:24 +0500

    Изменен тип Employee.Salary с int на decimal

--- ReportService/ReportService/Domain/Employee.cs
+++ ReportService/ReportService/Domain/Employee.cs
@@ -10,7 +10,7 @@ namespace ReportService.Domain
         public string Name { get; set; }
         public string Department { get; set; }
         public string  Inn { get; set; }
-        public int Salary { get; set; }
+        public decimal Salary { get; set; }
         public string BuhCode { get; set; }
     }
 }
