069613ad176bb224f7df3ad97186bf98a9100664
Roman Vasilyev <sektor_wins@mail.ru>
2020-02-02 23:09:02 +0500

    Добавлены выбросы эксепшеном для PeriodResolver.GetDate при некорректных входных параметрах

--- ReportService/ReportService/External/PeriodResolver.cs
+++ ReportService/ReportService/External/PeriodResolver.cs
@@ -16,6 +16,9 @@ namespace ReportService.External
         /// <returns>Дата в виде удобночитаемой строки</returns>
         public static string GetDate(int year, int month)
         {
+            if (month < 1 || month > 12) throw new ArgumentException("Укажите корректный месяц", nameof(month));
+            if (year < 1 || year > 9999) throw new ArgumentException("Укажите корректный год", nameof(year));
+
             return new DateTime(year, month, 1).ToString("MMMM yyyy", CultureInfo.CreateSpecificCulture("ru"));
         }
 
